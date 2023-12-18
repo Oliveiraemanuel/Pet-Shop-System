659b52a058ec7d02ff5bec4686cbb6a682be4690
Emanuel <emanuelolvr@outlook.com>
2023-12-17 22:20:45 -0300

    feat: insert variable email and bean validation

--- src/main/java/com/emanuel/vet/sistema/domains/vet/Vet.java
+++ src/main/java/com/emanuel/vet/sistema/domains/vet/Vet.java
@@ -1,6 +1,8 @@
 package com.emanuel.vet.sistema.domains.vet;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,9 +18,17 @@ public class Vet {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String firstName;
 
+    @NotBlank
     private String lastName;
 
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Column(unique = true)
     private String cfmv;
 }
