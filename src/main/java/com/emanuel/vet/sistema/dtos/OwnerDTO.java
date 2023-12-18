32263daa1f4fa84f0e0394c54d2b8d6a711c6e7f
Emanuel <emanuelolvr@outlook.com>
2023-12-17 22:30:02 -0300

    feat: create owner dto

---
+++ src/main/java/com/emanuel/vet/sistema/dtos/OwnerDTO.java
@@ -0,0 +1,4 @@
+package com.emanuel.vet.sistema.dtos;
+
+public record OwnerDTO(String firstName, String lastName, String adress, String city, String houseNumber, String email, String phoneNumber) {
+}
