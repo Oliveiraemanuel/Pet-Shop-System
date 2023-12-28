65b60a04387149cae74300db18dedd265e744668
Emanuel <emanuelolvr@outlook.com>
2023-12-27 23:45:59 -0300

    feat: insert @JoinColumn and remove constructor with dto

--- src/main/java/com/emanuel/vet/sistema/domains/pet/Pet.java
+++ src/main/java/com/emanuel/vet/sistema/domains/pet/Pet.java
@@ -2,11 +2,8 @@ package com.emanuel.vet.sistema.domains.pet;
 
 import com.emanuel.vet.sistema.domains.owner.Owner;
 import com.emanuel.vet.sistema.domains.pet.enums.Species;
-import com.emanuel.vet.sistema.dtos.PetDTO;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "pets")
@@ -20,6 +17,7 @@ public class Pet {
     private Long id;
 
     @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
 
     private String name;
@@ -27,11 +25,4 @@ public class Pet {
     private Species species;
 
     private String birthDate;
-
-    public Pet(PetDTO data) {
-        this.owner = data.owner();
-        this.name = data.name();
-        this.species = data.species();
-        this.birthDate = data.birthDate();
-    }
 }
