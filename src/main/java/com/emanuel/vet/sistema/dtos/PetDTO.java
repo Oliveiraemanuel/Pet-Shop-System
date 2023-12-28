ab362fc7488c3a122bee96c02838c83fbd7d034d
Emanuel <emanuelolvr@outlook.com>
2023-12-27 23:44:34 -0300

    feat: change type Owner to Long

--- src/main/java/com/emanuel/vet/sistema/dtos/PetDTO.java
+++ src/main/java/com/emanuel/vet/sistema/dtos/PetDTO.java
@@ -3,5 +3,5 @@ package com.emanuel.vet.sistema.dtos;
 import com.emanuel.vet.sistema.domains.owner.Owner;
 import com.emanuel.vet.sistema.domains.pet.enums.Species;
 
-public record PetDTO(Owner owner, String name, Species species, String birthDate) {
+public record PetDTO(Long ownerId, String name, Species species, String birthDate) {
 }
