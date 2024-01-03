572b17ac835eac05f965313c6b8f79a5d3f6db2b
Emanuel <emanuelolvr@outlook.com>
2024-01-03 00:21:34 -0300

    feat: crate vet producer

---
+++ src/main/java/com/emanuel/vet/sistema/producer/VetProducer.java
@@ -0,0 +1,31 @@
+package com.emanuel.vet.sistema.producer;
+
+import com.emanuel.vet.sistema.domains.vet.Vet;
+import com.emanuel.vet.sistema.dtos.EmailDTO;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VetProducer {
+
+    final RabbitTemplate rabbitTemplate;
+
+    public VetProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Value("${broker.queue.email.name}")
+    private String routingKey;
+
+    public void publishMessageEmail(Vet vet){
+
+        var emailDTO = new EmailDTO();
+
+        emailDTO.setTo(vet.getEmail());
+        emailDTO.setSubject("Seja Bem Vindo, " + vet.getFirstName() + "!");
+        emailDTO.setText("Você é muito valioso para nós");
+
+        rabbitTemplate.convertAndSend("", routingKey, emailDTO);
+    }
+}
